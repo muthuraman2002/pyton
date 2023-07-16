d1320325de556c413218c8ec9babbd940c4c398c
muthuraman2002 <128206014+muthuraman2002@users.noreply.github.com>
2023-07-16 11:41:08 +0530

    Add files via upload

---
+++ eb bill.py
@@ -0,0 +1,11 @@
+
+def bill(a):
+    r=4*a
+    print("Cost:",r)
+name=input("Enter the client name:")
+no=input("Enter the id of client:")
+x=int(input("Enter the units of client use:"))
+print("Name:",name)
+print("id:",no)
+
+bill(x)
